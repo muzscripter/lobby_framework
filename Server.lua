0516e76066788383bf9c32622258748e8ce2cf33
graverealm <109730545+muzscripter@users.noreply.github.com>
2023-09-17 13:01:29 -0400

    Update Server.lua

--- Server.lua
+++ Server.lua
@@ -2,6 +2,8 @@ local Lobbys = {}
 local PlayersLobby = {}
 local LobbyModule = require(game:GetService('ReplicatedStorage'):WaitForChild('Lobby'))
 local LobbyRemote = game:GetService('ReplicatedStorage'):WaitForChild('LobbyRemote')
+local PlayersOccupation = {}
+
 
 local function GenerateLobbyID()
 	return tostring(os.time()) .. math.random(1000, 9999)
@@ -25,9 +27,14 @@ function CreateLobby(Player, MaxPlayerAmount, PartyType, PartyName, ExtraInfo)
 	Template.Name = LobbyID
 	
 	Template.PartyName.Text = PartyName
-	Template.PlayerAmount.Text = 1 .. "/" .. MaxPlayerAmount
+	Template.PlayerAmount.Text = #Lobbys[LobbyID].Players .. "/" .. MaxPlayerAmount
 	Template.Parent = Player.PlayerGui.Frame.Background['Multi-Player'].Frame.Parties
 	Template.Visible = true
+
+	Lobbys[LobbyID]:SetLeader(Player)
+	Lobbys[LobbyID]:Add(Player)
+	
+	PlayersOccupation[Player.Name] = LobbyID
 	
 	return Lobbys[LobbyID]
 end
@@ -35,11 +42,24 @@ end
 LobbyRemote.OnServerEvent:Connect(function(Player: any, Argument: string, MaxPlayerAmount: number, PartyType, PartyName, ExtraInfo)
 	if Argument == 'Create' then
 		CreateLobby(Player, MaxPlayerAmount, PartyType, PartyName, ExtraInfo)
+		
 	elseif Argument == 'Join' then
-		Lobbys[ExtraInfo]:Add(Player)
+		Lobbys[ExtraInfo]:Add(Player, ExtraInfo)
+		PlayersOccupation[Player.Name] = ExtraInfo
+		
 	elseif Argument == 'Leave' then
-		Lobbys[ExtraInfo]:Leave(Player)
-	end 
+		Lobbys[ExtraInfo]:Leave(Player, ExtraInfo)
+		PlayersOccupation[Player.Name] = false
+		
+	elseif Argument == 'Disband' then
+		local ID = PlayersOccupation[Player.Name]
+
+		Lobbys[ID]:Disband()
+		for i,v in pairs(Lobbys) do
+			print('Index' .. i .. 'Variable' .. v)
+		end
+	end
+	
 end)
 
 
