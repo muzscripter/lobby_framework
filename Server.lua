bf7f5f88fff8f8a6c9ca42a1ce5a03e51314217b
graverealm <109730545+muzscripter@users.noreply.github.com>
2023-09-17 08:26:41 -0400

    Update Server.lua

--- Server.lua
+++ Server.lua
@@ -3,16 +3,51 @@ local PlayersLobby = {}
 local LobbyModule = require(game:GetService('ReplicatedStorage'):WaitForChild('Lobby'))
 local LobbyRemote = game:GetService('ReplicatedStorage'):WaitForChild('LobbyRemote')
 
-LobbyRemote.OnServerEvent:Connect(function(Player: any, Argument: string, Max: number, ExtraInfo)
+local function GenerateLobbyID()
+	return tostring(os.time()) .. math.random(1000, 9999)
+end
+
+function CreateLobby(Player, MaxPlayerAmount, PartyType, PartyName, ExtraInfo)
+	print(MaxPlayerAmount, PartyType, PartyName)
+	
+	local LobbyID = GenerateLobbyID()
+	
+	Lobbys[LobbyID] = LobbyModule.Create({
+		MaxPlayers = MaxPlayerAmount,
+		Type = PartyType,
+		Name = PartyName,
+		ID = LobbyID
+	})
+	
+	task.wait()
+	
+	local Template = game:GetService('ReplicatedStorage').PartyTemplate
+	Template.Name = LobbyID
+	
+	Template.PartyName.Text = PartyName
+	Template.PlayerAmount.Text = 1 .. "/" .. MaxPlayerAmount
+	Template.Parent = Player.PlayerGui.Frame.Background['Multi-Player'].Frame.Parties
+	Template.Visible = true
+	
+	return Lobbys[LobbyID]
+end
+
+LobbyRemote.OnServerEvent:Connect(function(Player: any, Argument: string, MaxPlayerAmount: number, PartyType, PartyName, ExtraInfo)
 	if Argument == 'Create' then
-		Lobbys[Player.Name] = LobbyModule.Create({
-			Players = {},
-			Admin = Player,
-			MaxPlayers = Max
-		})
+		CreateLobby(Player, MaxPlayerAmount, PartyType, PartyName, ExtraInfo)
 	elseif Argument == 'Join' then
 		Lobbys[ExtraInfo]:Add(Player)
 	elseif Argument == 'Leave' then
 		Lobbys[ExtraInfo]:Leave(Player)
-	end
+	end 
 end)
+
+
+game:GetService('ReplicatedStorage'):WaitForChild('GrabParty').OnServerInvoke = function(Player, ID)
+	local Lobby = Lobbys[ID]
+	if Lobby then
+		print(Lobby)
+	else
+		print('Failed to get table')
+	end
+end
